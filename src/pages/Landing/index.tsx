80d3aa7ab75bbbbdf7b619b21a4c4338a7237ee3
auriliosantos <auriliosantos@hotmail.com.br>
2020-08-18 19:44:55 -0300

    Configurada tela de login e adicionada logo

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,16 +1,91 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Animated,
+  Alert,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import api from "../../services/api";
 
+import logoImg from "../../assets/images/logo.png";
 import styles from "./styles";
 
 function Landing() {
+  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 95 }));
+  const [opacity] = useState(new Animated.Value(0));
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   const { navigate } = useNavigation();
+
+  async function handleSubmit() {
+    try {
+      const response = await api.post("/auth/login", {
+        name,
+        password,
+      });
+      console.log(response.data);
+      navigate("CRUDTabs", response.data);
+    } catch (err) {
+      Alert.alert("Erro no login", "Nome ou senha incorretos, tente novamente");
+    }
+
+    /* navigate("CRUDTabs"); */
+  }
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.spring(offset.y, {
+        useNativeDriver: true,
+        toValue: 0,
+        speed: 4,
+        bounciness: 20,
+      }),
+      Animated.timing(opacity, {
+        useNativeDriver: true,
+        toValue: 1,
+        duration: 800,
+      }),
+    ]).start();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Landing</Text>
-      <Button title="Acessar" onPress={() => navigate("CRUDTabs")} />
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image source={logoImg} />
+      </View>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: opacity,
+            transform: [{ translateY: offset.y }],
+          },
+        ]}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          autoCorrect={false}
+          onChangeText={(text) => setName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity
+          style={styles.btnSubmit}
+          onPress={() => handleSubmit()}
+        >
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 }
 
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,10 +1,45 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#023E8A",
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "90%",
+  },
+  input: {
+    backgroundColor: "#fff",
+    height: 50,
+    width: "90%",
+    marginBottom: 15,
+    color: "#BCBDC0",
+    fontSize: 17,
+    fontFamily: "Roboto_400Regular",
+    borderRadius: 8,
+    padding: 10,
+  },
+  btnSubmit: {
+    backgroundColor: "#C2E812",
+    width: "90%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+  submitText: {
+    fontFamily: "Roboto_700Bold",
+    color: "#023E8A",
+    fontSize: 18,
   },
 });
 
